268e91e002d871a52ff4c70a6e3df9a94ca761d1
akidon0000 <a@art.jp>
2023-07-24 15:30:43 +0900

    init commit

---
+++ Camera_App/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Camera_App
+//
+//  Created by Akihiro Matsuyama on 2023/07/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
