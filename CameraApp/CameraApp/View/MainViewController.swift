6a70bf67200c976e7ba96e3392f3d3b6e8840c1f
akidon0000 <a@art.jp>
2023-07-26 09:28:31 +0900

    [Add]takePhotoButtonを追加

--- CameraApp/CameraApp/View/MainViewController.swift
+++ CameraApp/CameraApp/View/MainViewController.swift
@@ -9,21 +9,30 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    // MARK: - IBOutlet
+
+    @IBOutlet weak var takePhotoButton: UIButton!
+    @IBOutlet weak var takePhotoButtonWidthConstraint: NSLayoutConstraint!
+
+    // MARK: - View Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTakePhotoButton()
     }
-    
 
-    /*
-    // MARK: - Navigation
+    // MARK: - IBAction
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func takePhotoButton(_ sender: Any) {
     }
-    */
 
+    // MARK: - Methods [Private]
+
+    private func setupTakePhotoButton() {
+        takePhotoButton.setTitle(NSLocalizedString("agreement button", comment: ""), for: .normal)
+        takePhotoButton.tintColor = .white
+        takePhotoButton.backgroundColor = .blue
+        takePhotoButton.layer.cornerRadius = 10.0
+        takePhotoButtonWidthConstraint.constant = view.frame.width / 2
+    }
 }
