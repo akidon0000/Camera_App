3dfe19cde0bcc245951e1ce1857b1036cb6dbda0
akidon0000 <a@art.jp>
2023-07-27 11:43:50 +0900

    [Add]カメラ起動等の機能を追加

--- CameraApp/CameraApp/View/CameraViewController.swift
+++ CameraApp/CameraApp/View/CameraViewController.swift
@@ -7,15 +7,35 @@
 
 import UIKit
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     static func instantiate() -> CameraViewController {
         return UIStoryboard(name: "Camera", bundle: nil).instantiateInitialViewController() as! CameraViewController
     }
 
+    @IBOutlet weak var imageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+
+    @IBAction func launchCamera(_ sender: Any) {
+        let camera = UIImagePickerController.SourceType.camera
+        if UIImagePickerController.isSourceTypeAvailable(camera) {
+            let picker = UIImagePickerController()
+            picker.sourceType = camera
+            picker.delegate = self
+            self.present(picker,animated: true)
+        }
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController,
+                                       didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        self.imageView.image = image
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        self.dismiss(animated: true)
     }
 
 }
