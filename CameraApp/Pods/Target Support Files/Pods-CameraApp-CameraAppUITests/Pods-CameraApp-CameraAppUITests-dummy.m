860c4522e9932030cc86fc670b570534e6b2c67f
akidon0000 <a@art.jp>
2023-07-27 22:14:56 +0900

    [Add]pod install

---
+++ CameraApp/Pods/Target Support Files/Pods-CameraApp-CameraAppUITests/Pods-CameraApp-CameraAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CameraApp_CameraAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CameraApp_CameraAppUITests
+@end
---
+++ CameraApp/Pods/Target Support Files/Pods-CameraAppTests/Pods-CameraAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CameraAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_CameraAppTests
+@end
