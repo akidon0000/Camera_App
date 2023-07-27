2776513b411e198fb5929c94b6eb5ba83011b494
akidon0000 <a@art.jp>
2023-07-27 23:38:15 +0900

    [Fix] nibにカメラ表示成功

--- CameraApp/CameraApp/View/CameraView.swift
+++ CameraApp/CameraApp/View/CameraView.swift
@@ -8,98 +8,45 @@
 import UIKit
 import AVFoundation
 
-class CameraView: UIView {
+class CameraView: UIView, AVCapturePhotoCaptureDelegate {
 
-    // MARK: Stored Instance Properties
+    @IBAction func button(_ sender: Any) {
+        print("bbb")
+    }
 
-    // MARK: Computed Instance Properties
+    //コードで設置した時
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNib()
+    }
 
-    // MARK: IBOutlets
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        loadNib()
+    }
 
-    // MARK: Initializers
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //viewのautolayout取得前//VCの制約決定前
+    }
 
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//
-//        configureView()
-//    }
-//
-//    required init?(coder aDecoder: NSCoder) {
-//        super.init(coder: aDecoder)
-//
-//        configureView()
-//    }
-//
-//    // MARK: IBActions
-//
-//    // MARK: Other Internal Methods
-//
-//    override func prepareForInterfaceBuilder() {
-//        super.prepareForInterfaceBuilder()
-//
-//        configureView()
-//    }
-//
-//    // MARK: Other Private Methods
-//
-//    private func configureView() {
-//        loadNib()
-//
-//        // TODO: 他にUIの装飾処理があれば実行する
-//    }
-//
-//    private func loadNib() {
-//        guard let cameraView = R.nib.cameraView(owner: self) else {
-//            fatalError("Fail to load FooView from Nib.")
-//        }
-//        cameraView.frame = self.bounds
-//        addSubview(cameraView)
-//    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        //viewのautolayout取得前後//VCの制約決定前
+    }
 
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-////        configure()
-//        let view = UINib(nibName: "CameraView", bundle: nil).instantiate(withOwner: self, options: nil).first as! UIView
-//    }
-//
-//    required init?(coder aDecoder: NSCoder) {
-//        super.init(coder: aDecoder)
-//        configure()
-//    }
-//
-//    private func configure() {
-////        let nib = UINib(nibName: "CameraView", bundle: nil)
-////        guard let view = nib.instantiate(withOwner: self, options: nil).first as? UIView else { return }
-////        addSubview(view)
-//    }
-//    //プロパティは省略
-//    //storyboardで設置した時
-//    init() {
-//        super.init(frame: .zero)
-//        let view = Bundle.main.loadNibNamed("CameraView", owner: self, options: nil)?.first as! UIView
-//        let view = UINib(nibName: "CameraView", bundle: nil).instantiate(withOwner: self, options: nil).first as! UIView
-////        view.frame = self.bounds
-//        print(view.frame)
-//        print(self.bounds)
-//        addSubview(view)
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        //viewのautolayout取得前//VCの制約決定前
-//    }
+    private func loadNib() {
+        settingSession()
+        startCapture()
+    }
 
-//    var captureVideoLayer: AVCaptureVideoPreviewLayer!
-//
-//    var cameraView: UIView!
-//    var cameraDevices: AVCaptureDevice!
-//
-//    var captureSession = AVCaptureSession()
-//    var imageOutput = AVCapturePhotoOutput()
+    var captureVideoLayer: AVCaptureVideoPreviewLayer!
+
+    var cameraView: UIView!
+    var cameraDevices: AVCaptureDevice!
+
+    var captureSession = AVCaptureSession()
+    var imageOutput = AVCapturePhotoOutput()
 
 //    override func viewDidLoad() {
 //        super.viewDidLoad()
@@ -140,45 +87,45 @@ class CameraView: UIView {
 //    }
 
 
-//    private func settingSession() {
-//        // カメラの設定やセッションの組み立てはここで行う
-//        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera],
-//                                                                      mediaType: AVMediaType.video,
-//                                                                      position: AVCaptureDevice.Position.unspecified)
-//        // プロパティの条件を満たしたカメラデバイスの取得（バックカメラ）
-//        let devices = deviceDiscoverySession.devices
-//        for device in devices {
-//            if device.position == AVCaptureDevice.Position.back {
-//                cameraDevices = device
-//            }
-//        }
-//        //バックカメラからVideoInputを取得
-//        let videoInput: AVCaptureInput!
-//        do {
-//            videoInput = try AVCaptureDeviceInput.init(device: cameraDevices)
-//        } catch {
-//            videoInput = nil
-//        }
-//
-//        //出力先を生成
-//        imageOutput = AVCapturePhotoOutput()
-//        //セッションに追加
-//        captureSession.addInput(videoInput)
-//        captureSession.addOutput(imageOutput)
-//        //画像を表示するレイヤーを生成
-//        captureVideoLayer = AVCaptureVideoPreviewLayer.init(session: captureSession)
-//        captureVideoLayer.frame = self.bounds
-//        captureVideoLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
-//
-//        //Viewに追加
-//        self.layer.insertSublayer(captureVideoLayer, at: 0)
-//    }
-//
-//    private func startCapture() {
-//        captureSession.startRunning()
-//    }
-//
-//    private func stopCapture() {
-//        captureSession.stopRunning()
-//    }
+    private func settingSession() {
+        // カメラの設定やセッションの組み立てはここで行う
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera],
+                                                                      mediaType: AVMediaType.video,
+                                                                      position: AVCaptureDevice.Position.unspecified)
+        // プロパティの条件を満たしたカメラデバイスの取得（バックカメラ）
+        let devices = deviceDiscoverySession.devices
+        for device in devices {
+            if device.position == AVCaptureDevice.Position.back {
+                cameraDevices = device
+            }
+        }
+        //バックカメラからVideoInputを取得
+        let videoInput: AVCaptureInput!
+        do {
+            videoInput = try AVCaptureDeviceInput.init(device: cameraDevices)
+        } catch {
+            videoInput = nil
+        }
+
+        //出力先を生成
+        imageOutput = AVCapturePhotoOutput()
+        //セッションに追加
+        captureSession.addInput(videoInput)
+        captureSession.addOutput(imageOutput)
+        //画像を表示するレイヤーを生成
+        captureVideoLayer = AVCaptureVideoPreviewLayer.init(session: captureSession)
+        captureVideoLayer.frame = self.bounds
+        captureVideoLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+
+        //Viewに追加
+        self.layer.insertSublayer(captureVideoLayer, at: 3)
+    }
+
+    private func startCapture() {
+        captureSession.startRunning()
+    }
+
+    private func stopCapture() {
+        captureSession.stopRunning()
+    }
 }
--- CameraApp/CameraApp/View/ProfileViewController.swift
+++ CameraApp/CameraApp/View/ProfileViewController.swift
@@ -11,10 +11,6 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        var view = UIView()
-////        view = R.nib.cameraView
-//        view.custom
-//        self.view.addSubview(view)
         if let cameraView = R.nib.cameraView.firstView(withOwner: nil) as? CameraView {
             self.view.addSubview(cameraView)
         }
